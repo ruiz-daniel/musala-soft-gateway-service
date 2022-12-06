1ee3586914129fb1f75c453ea2e1cd68543785d4
DRGWeiss <ruizd1710@gmail.com>
2022-12-06 12:26:16 -0500

    in memory sqlite database

--- express/app.js
+++ express/app.js
@@ -1,28 +1,100 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors')
+const crypto = require ('crypto')
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
-const cors = require("cors");
+// const database = require('./sqlitedb')
 
-app.use(cors());
-app.listen(port, console.log(`Server started on port ${port}`));
+// Database
+const connect = require('@databases/sqlite')
+const { sql } = require('@databases/sqlite')
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const db = connect()
 
-app.get("/gateways", (req, res) => {
-  res.send({
-    name: 'test',
-    ip: '10.2.45.253',
-    serial: '129581sdlkhja'
-  })
+async function prepare() {
+  await db.query(sql`
+    CREATE TABLE gateway (
+      serial VARCHAR NOT NULL PRIMARY KEY,
+      name VARCHAR NOT NULL,
+      ip VARCHAR NOT NULL
+    );
+  `)
+}
+const prepared = prepare()
+
+async function set(serial, name, ip) {
+  await prepared
+  await db.query(sql`
+    INSERT INTO gateway (serial, name, ip)
+      VALUES (${serial}, ${name}, ${ip})
+    ON CONFLICT (serial) DO UPDATE
+      SET name=excluded.name;
+      SET ip=excluded.ip
+  `)
+}
+
+async function getAll() {
+  await prepared
+  const results = await db.query(sql`SELECT * FROM gateway;`)
+  if (results.length) {
+    return results
+  } else {
+    return undefined
+  }
+}
+
+async function getBySerial(serial) {
+  await prepared
+  const results = await db.query(sql`
+    SELECT * FROM gateway WHERE serial=${serial};
+  `)
+  if (results.length) {
+    return results[0]
+  } else {
+    return undefined
+  }
+}
+
+async function remove(serial) {
+  await prepared
+  await db.query(sql`
+    DELETE FROM gateway WHERE serial=${serial};
+  `)
+}
+
+async function run() {
+  await set(crypto.randomUUID(), 'Gateway1', '192.168.1.1')
+  await set(crypto.randomUUID(), 'Gateway2', '192.168.1.2')
+  await set(crypto.randomUUID(), 'Gateway3', '192.168.1.3')
+  await set(crypto.randomUUID(), 'Gateway4', '192.168.1.4')
+  await set(crypto.randomUUID(), 'Gateway5', '192.168.1.5')
+}
+run().catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
 })
-  
-app.post("/post", (req, res) => {
-  console.log("Connected to React");
-  res.send("Test");
-});
-  
+//...........................................................
 
+app.use(cors())
+app.listen(port, console.log(`Server started on port ${port}`))
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/gateways', async (req, res) => {
+  const response = await getAll()
+  res.send(response)
+})
+
+app.get('/gateway', async (req, res) => {
+  const response = await getBySerial(req.query.serial)
+  console.log(response)
+  res.send(response)
+})
+
+app.post('/gateway', (req, res) => {
+  console.log('Connected to React')
+  res.send('Test')
+})
---
+++ express/sqlitedb.js
@@ -0,0 +1,60 @@
+const database = () => {
+  const connect = require('@databases/sqlite')
+  const { sql } = require('@databases/sqlite')
+
+  const db = connect()
+
+  async function prepare() {
+    await db.query(sql`
+    CREATE TABLE app_data (
+      id VARCHAR NOT NULL PRIMARY KEY,
+      value VARCHAR NOT NULL
+    );
+  `)
+  }
+  const prepared = prepare()
+
+  async function set(id, value) {
+    await prepared
+    await db.query(sql`
+    INSERT INTO app_data (id, value)
+      VALUES (${id}, ${value})
+    ON CONFLICT (id) DO UPDATE
+      SET value=excluded.value;
+  `)
+  }
+
+  async function get(id) {
+    await prepared
+    const results = await db.query(sql`
+    SELECT value FROM app_data WHERE id=${id};
+  `)
+    if (results.length) {
+      return results[0].value
+    } else {
+      return undefined
+    }
+  }
+
+  async function remove(id) {
+    await prepared
+    await db.query(sql`
+    DELETE FROM app_data WHERE id=${id};
+  `)
+  }
+
+  async function run() {
+    console.log(await get('name'))
+    await set('name', 'Forbes')
+    console.log(await get('name'))
+    await set('name', 'Forbes Lindesay')
+    console.log(await get('name'))
+    remove('name')
+  }
+  run().catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
+}
+
+export default database
