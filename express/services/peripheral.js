af2c0f9f3afa6a67c046b8a682a8cc7b38cfe3f2
DRGWeiss <ruizd1710@gmail.com>
2022-12-07 17:28:32 -0500

    complete peripheral service

--- express/services/peripheral.js
+++ express/services/peripheral.js
@@ -14,4 +14,33 @@ module.exports.handler = {
     const result = await peripheralModel.find()
     return result
   },
+
+  async find(id) {
+    const result = await peripheralModel.findById(id)
+    return result
+  },
+
+  // http patch update
+  async update(peripheral) {
+    if (!peripheral) throw new Error('Missing peripheral')
+
+    const result = await peripheralModel.findByIdAndUpdate(peripheral._id, peripheral)
+    return result
+  },
+  
+  // http put update
+  async updateOverride(peripheral) {
+    if (!peripheral) throw new Error('Missing peripheral')
+
+    const doc = await peripheralModel.findOne({_id: peripheral._id})
+
+    doc.overwrite(peripheral)
+    const result = await doc.save()
+    return result
+  },
+
+  async delete(id) {
+    const result = await peripheralModel.findByIdAndDelete(id)
+    return result
+  }
 }
