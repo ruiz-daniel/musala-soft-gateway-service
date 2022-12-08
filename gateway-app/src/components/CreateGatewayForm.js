f89b9c8273ed6acddd1c75b7cf6c2a2a27d24252
DRGWeiss <ruizd1710@gmail.com>
2022-12-08 15:09:40 -0500

    new gateway form and adjustments

---
+++ gateway-app/src/components/CreateGatewayForm.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+import { InputText } from 'primereact/inputtext';
+
+const CreateGatewayForm = ({ name, setName, ip, setIP }) => {
+  return(
+    <>
+    <div className='flex flex-column'>
+      <h4>Name</h4>
+      <InputText value={name} onChange={(e) => setName(e.target.value) } />
+      <h4>IP</h4>
+      <InputText value={ip} onChange={(e) => setIP(e.target.value) } />
+    </div>
+    </>
+  )
+}
+
+export default CreateGatewayForm
--- gateway-app/src/pages/Gateways.js
+++ gateway-app/src/pages/Gateways.js
@@ -6,11 +6,19 @@ import { useNavigate } from 'react-router-dom'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 import { Button } from 'primereact/button'
+import { Dialog } from 'primereact/dialog'
+
+import CreateGatewayForm from '../components/CreateGatewayForm'
 
 const Gateways = () => {
   const navigate = useNavigate()
   const [gateways, setGateways] = useState([])
   const [expandedRows, setExpandedRows] = useState(null)
+  const [addModal, toggleAddModal] = useState(false)
+
+  // new gateway fields
+  const [name, setName] = useState("")
+  const [ip, setIP] = useState("")
 
   useEffect(() => {
     api.getGateways(handleGateways)
@@ -43,13 +51,27 @@ const Gateways = () => {
   }
 
   const handleDetails = (rowData) => {
-    navigate(`/gateway?serial=${rowData.serial}`)
+    navigate(`/gateway?id=${rowData._id}`)
   }
 
   const handleDelete = (rowData) => {
-    api.deleteGateway(rowData.serial, (data) => {
+    api.deleteGateway(rowData._id, (data) => {
       setGateways(data)
     })
+  }
+  const handleCreate = () => {
+    if (name?.length && ip?.length) {
+      api.createGateway({name, ip}, onCreate)
+    }
+  }
+
+  const onCreate = (data) => {
+    gateways.push(data)
+    toggleAddModal(false)
+  }
+
+  const hide = () => {
+    toggleAddModal(false)
   }
 
   const allowExpansion = (rowData) => {
@@ -61,14 +83,21 @@ const Gateways = () => {
     return rowData.status ? <p>Online</p> : <p>Offline</p>
   }
 
+  const footer = (
+    <div>
+      <Button label="Create" icon="pi pi-check" onClick={handleCreate} />
+      <Button label="Cancel" icon="pi pi-times" onClick={hide} />
+    </div>
+  )
+
   const rowExpansionTemplate = (data) => {
     return (
-      <div className='px-5'>
+      <div className="px-5">
         <h5>Peripherals</h5>
         <DataTable value={data.peripherals} responsiveLayout="scroll">
-          <Column field="id" header="Id" sortable></Column>
+          <Column field="uid" header="UID" sortable></Column>
           <Column field="vendor" header="Vendor" sortable></Column>
-          <Column field="created_date" header="Created" sortable></Column>
+          <Column field="date" header="Created" sortable></Column>
           <Column
             field="status"
             header="Status"
@@ -82,7 +111,24 @@ const Gateways = () => {
 
   return (
     <div className="p-4">
-      <h2 className="mb-3">List of gateways</h2>
+      <h2 className="mb-3">
+        List of gateways{' '}
+        <Button
+          icon="pi pi-plus"
+          className="p-button-rounded p-button-sm"
+          onClick={() => toggleAddModal(true)}
+        />
+      </h2>
+      <Dialog
+        header="New Gateway"
+        footer={footer}
+        visible={addModal}
+        style={{ width: '40vw' }}
+        modal
+        onHide={hide}
+      >
+        <CreateGatewayForm name={name} ip={ip} setName={setName} setIP={setIP} />
+      </Dialog>
 
       {gateways && (
         <DataTable
